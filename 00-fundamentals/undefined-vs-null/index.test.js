d914d88fe338ea56aa8fe76c8a6ff4e33271b357
Reynaldo Navedo <codigoderey@outlook.com>
2024-07-30 12:34:45 -0400

    minor updates

--- 00-fundamentals/undefined-vs-null/index.test.js
+++ 00-fundamentals/undefined-vs-null/index.test.js
@@ -7,7 +7,7 @@ describe("Understanding null and undefined", () => {
 		expect(fnTrue(x, y)).toBe(true);
 	});
 
-	test("Should return true when comparing null and undefined", () => {
+	test("Should return false when comparing null and undefined", () => {
 		x = null;
 		y = undefined;
 		expect(fnFalse(x, y)).toBe(false);
