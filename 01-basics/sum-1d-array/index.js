1b62056a156dc7b950c5b92b65cd516060e242a8
Reynaldo Navedo <codigoderey@outlook.com>
2024-08-28 14:25:08 -0400

    ppt initial added

--- 01-basics/sum-1d-array/index.js
+++ 01-basics/sum-1d-array/index.js
@@ -7,18 +7,31 @@
  * @return {number[]}
 **/
 
+// solution #1 using reduce
+// function fn(arrNums){
 
+//     var result = []
+    
+//     arrNums.reduce((acc, curr, index) => {
+//         result[index] = acc + curr
+//         return result[index]
+//     }, 0)
+    
+//     return result;
+
+// }
+
+// solution #2 using for loop
 function fn(arrNums){
+    let result = []
+    result[0] = arrNums[0]
 
-    var result = []
-    
-    arrNums.reduce((acc, curr, index) => {
-        result[index] = acc + curr
-        return result[index]
-    }, 0)
-    
-    return result;
+    for(i = 1; i < arrNums.length; i++){
+        result[i] = arrNums[i] + result[i - 1]
+    }
 
+    console.log(result)
+    return result
 }
 
 fn([1, 2, 3, 4, 5])
