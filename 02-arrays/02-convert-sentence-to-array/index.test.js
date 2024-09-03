24d1137503f6943147fede0c399aaf0d0b694006
Reynaldo Navedo <codigoderey@outlook.com>
2024-09-02 21:21:48 -0400

    minor update to an array challenge

--- 02-arrays/02-convert-sentence-to-array/index.test.js
+++ 02-arrays/02-convert-sentence-to-array/index.test.js
@@ -1,5 +1,11 @@
 const fn = require('./index.js');
 
-test('converts a sentence into an array of words', () => {
-    expect(fn("The quick brown fox jumps over the lazy dog.")).toEqual(["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog."]);
+describe("Convert a sentence into an array of words", () => {
+    test("It should return an array of words in the sentence", () => {
+        expect(fn("The quick brown fox jumps over the lazy dog.")).toEqual(["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog."]);
+    });
+
+    test("It should return an array of words in the sentence", () => {
+        expect(fn("Nice work learning arrays.")).toEqual(["Nice", "work", "learning", "arrays."]);
+    });
 });
